85cf66e806c121efeaeee568892dd6aabdedec9a
13Kirat <141816179+13Kirat@users.noreply.github.com>
2024-12-27 05:19:22 +0530

    day-1 completed

---
+++ day-1/exrecise-4.py
@@ -0,0 +1,11 @@
+a = input(" Enter the value of a: ")
+b = input(" Enter the value of b: ")
+
+print(f"a: {a}")
+print(f"b: {b}")
+print("After Swaping : ")
+temp = a
+a = b
+b = temp
+print(f"a: {a}")
+print(f"b: {b}")
---
+++ day-1/variables.py
@@ -0,0 +1,16 @@
+name = input("What is your name ? ")
+print(name)
+
+name = "Kirat"
+print(name)
+
+name = input("What is your name ? ")
+length = len(name)
+print(length)
+
+# naming convention
+# snake_case
+# camelCase
+# PascalCase
+# kebab-case
+# train-case
